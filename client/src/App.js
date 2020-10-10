d780b2235ec592d20b67ff4b19ab188d3c70ddb1
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-10 18:24:49 +0200

    added edit component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Admin, Resource} from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import PostList from './components/PostList';
+import PostEdit from './components/PostEdit';
 import restProvider from 'ra-data-simple-rest';
 import PostCreate from './components/PostCreate';
-
 
 function App() {
   return (
     // ! localhost:3000, NOT 5000 because I added a proxy
     <Admin dataProvider={restProvider('http://localhost:3000')}>
-      <Resource name="posts" list={PostList} create={PostCreate} />
+      <Resource
+        name="posts"
+        list={PostList}
+        create={PostCreate}
+        edit={PostEdit}
+      />
     </Admin>
   );
 }
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import {List, Datagrid, DateField, TextField, EditButton, DeleteButton} from 'react-admin';
-
+import React from 'react';
+import {
+  List,
+  Datagrid,
+  DateField,
+  TextField,
+  EditButton,
+  DeleteButton,
+} from 'react-admin';
 
 const PostList = (props) => {
   return (
     <List {...props}>
       <Datagrid>
-        <TextField source='id' />
-        <TextField source='title' />
-        <EditButton basePath='/posts' />
-        <DateField source='publishedAt' />
-        <DeleteButton basePath='/posts' />
+        <TextField source="id" />
+        <TextField source="title" />
+        <DateField source="publishedAt" />
+        <EditButton basePath="/posts" />
+        <DeleteButton basePath="/posts" />
       </Datagrid>
     </List>
-  )
-}
+  );
+};
 
-export default PostList
+export default PostList;
