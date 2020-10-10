db399210006f587ef8d6fe79866c7ff4a42236a5
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-10 17:27:47 +0200

    added React Admin root component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Admin, Resource} from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    // ! localhost:3000, NOT 5000 because I added a proxy
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+    </Admin>
   );
 }
 
