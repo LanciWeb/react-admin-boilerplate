9ec57e613a1721f736725d81a2620a0f384f5ed5
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-10 18:16:39 +0200

    Added Creation

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Admin, Resource} from 'react-admin';
 import PostList from './components/PostList';
 import restProvider from 'ra-data-simple-rest';
+import PostCreate from './components/PostCreate';
+
 
 function App() {
   return (
     // ! localhost:3000, NOT 5000 because I added a proxy
     <Admin dataProvider={restProvider('http://localhost:3000')}>
-      <Resource name="posts" list={PostList} />
+      <Resource name="posts" list={PostList} create={PostCreate} />
     </Admin>
   );
 }
