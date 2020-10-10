aeea9c95c3e9927d2b91b49a3e20f923c03dd694
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-10 18:08:03 +0200

    fixed proxy

--- middlewares/range.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, response, next) => {
-  //? this is a header needed by react admin in the response
-  //! the second argument should be calculated by the backend and represents pagination data
-  response.header('Content-Range', 'posts 0-20/20')
-  next();
-}
