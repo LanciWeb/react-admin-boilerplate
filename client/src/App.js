b89d79fc9acb5f0dfb176bd862a1964b5a5bb3d7
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-10 18:34:08 +0200

    added user resource

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import PostList from './components/PostList';
 import PostEdit from './components/PostEdit';
+import UserList from './components/UserList';
+import UserEdit from './components/UserEdit';
 import restProvider from 'ra-data-simple-rest';
 import PostCreate from './components/PostCreate';
+import UserCreate from './components/UserCreate';
 
 function App() {
   return (
@@ -14,6 +17,12 @@ function App() {
         list={PostList}
         create={PostCreate}
         edit={PostEdit}
+      />
+      <Resource
+        name="users"
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
       />
     </Admin>
   );
